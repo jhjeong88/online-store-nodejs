2d8e84e316d1b3b18a94f9426d6ba9e6b581cf2f
jhejong88 <jhjeong0316@gmail.com>
2019-01-03 19:58:26 +0900

    HW#2

--- routes/contacts.js
+++ routes/contacts.js
@@ -26,3 +26,34 @@ router.post('/write', (req, res) => {
         res.redirect('/contacts');
     });
 });
+
+router.get('/delete/:id', (req, res) =>{
+    ContactsModel.remove({id:req.params.id}, (err) => {
+        res.redirect('/contacts');
+    })
+});
+
+router.get('/detail/:id', (req, res) => {
+    ContactsModel.findOne({id:req.params.id}, (err, contact)=> {
+        res.render('contacts/contactsDetail', {'contact': contact});
+    });
+});
+
+router.get('/edit/:id', (req, res) => {
+    ContactsModel.findOne({id:req.params.id}, (err, contact)=> {
+        res.render('contacts/form', {'contact': contact});
+    });
+});
+
+router.post('/edit/:id', (req, res) => {
+    const query = {
+        name: req.body.name,
+        phone: req.body.phone,
+        age: req.body.age,
+        email: req.body.email
+    }
+
+    ContactsModel.update({id: req.params.id}, {$set:query}, (err) => {
+        res.redirect('/contacts');
+    });
+});
